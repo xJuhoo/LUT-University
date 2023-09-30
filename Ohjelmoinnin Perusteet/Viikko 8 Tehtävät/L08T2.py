d61c5d96e4006dcdba1adac38ae85e18d0f1aede
Juho Rekonen <128222910+xJuhoo@users.noreply.github.com>
2023-09-30 12:40:11 +0300

    Update L08T2.py

--- Ohjelmoinnin Perusteet/Viikko 8 Tehtävät/L08T2.py
+++ Ohjelmoinnin Perusteet/Viikko 8 Tehtävät/L08T2.py
@@ -1,19 +1,3 @@
-######################################################################
-# CT60A0203 Ohjelmoinnin perusteet
-# Tekijä: Juho Rekonen
-# Opiskelijanumero:441410
-# Päivämäärä: 6.11.2022
-# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat
-# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
-#
-# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse
-# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
-# vaikuttaneet siihen yllä mainituilla tavoilla.
-######################################################################
-# Tehtävä L08T2.py
-
-# eof
-
 import L08T2Kirjasto
 
 def valikko():
