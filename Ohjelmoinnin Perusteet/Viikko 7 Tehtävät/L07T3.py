91462545440e466cd2a704239887af6d03a4d657
Juho Rekonen <128222910+xJuhoo@users.noreply.github.com>
2023-09-30 12:39:22 +0300

    Update L07T3.py

--- Ohjelmoinnin Perusteet/Viikko 7 Tehtävät/L07T3.py
+++ Ohjelmoinnin Perusteet/Viikko 7 Tehtävät/L07T3.py
@@ -1,19 +1,3 @@
-######################################################################
-# CT60A0203 Ohjelmoinnin perusteet
-# Tekijä: Juho Rekonen
-# Opiskelijanumero: 441410
-# Päivämäärä: 20.10.2022
-# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat
-# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
-#
-# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse
-# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
-# vaikuttaneet siihen yllä mainituilla tavoilla.
-######################################################################
-# Tehtävä L07T3.py
-
-# eof
-
 def paaohjelma():
     tiedosto = input("Anna tiedoston nimi: ")
     luenta = open(tiedosto, "r", encoding = "utf-8")
