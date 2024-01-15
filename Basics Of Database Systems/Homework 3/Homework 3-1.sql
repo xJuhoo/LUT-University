79f8d0e98756c6016b7c0cb27c5c26c53fa95801
Juho Rekonen <128222910+xJuhoo@users.noreply.github.com>
2024-01-15 19:43:45 +0200

    Add files via upload

---
+++ Basics Of Database Systems/Homework 3/Homework 3-1.sql
@@ -0,0 +1,53 @@
+CREATE TABLE "Comments" (
+    "CommentID" INTEGER NOT NULL,
+    "UserID" INTEGER NOT NULL,
+    "TweetID" INTEGER,
+    "FK_CommentID" INTEGER,
+    "Content" VARCHAR(200),
+    PRIMARY KEY("CommentID"),
+    FOREIGN KEY("UserID") REFERENCES "User"("UserID"),
+    FOREIGN KEY("TweetID") REFERENCES "Tweet"("TweetID"),
+    FOREIGN KEY("CommentID") REFERENCES "Comments"("CommentID")
+);
+
+CREATE TABLE "Likes" (
+    "LikeID" INTEGER NOT NULL,
+    "UserID" INTEGER NOT NULL,
+    "TweetID" INTEGER,
+    "CommentID" INTEGER,
+    PRIMARY KEY("LikeID"),
+    FOREIGN KEY("UserID") REFERENCES "User"("UserID"),
+    FOREIGN KEY("TweetID") REFERENCES "Tweet"("TweetID"),
+    FOREIGN KEY("CommentID") REFERENCES "Comments"("CommentID")
+);
+
+CREATE TABLE "User" (
+    "UserID" INTEGER NOT NULL,
+    "Username" VARCHAR(50),
+    "Verified" VARCHAR(10),
+    "Followers" INTEGER,
+    PRIMARY KEY("UserID")
+);
+
+CREATE TABLE "Tweet" (
+    "TweetID" INTEGER NOT NULL,
+    "UserID" INTEGER NOT NULL,
+    "Content" VARCHAR(200),
+    PRIMARY KEY("TweetID"),
+    FOREIGN KEY("UserID") REFERENCES "User"("UserID")
+);
+
+CREATE TABLE "HashtagsInContent" (
+    "HashtagID" INTEGER NOT NULL,
+    "TweetID" INTEGER,
+    "CommentID" INTEGER,
+    FOREIGN KEY("HashtagID") REFERENCES "Hashtag"("HashtagID"),
+    FOREIGN KEY("TweetID") REFERENCES "Tweet"("TweetID"),
+    FOREIGN KEY("CommentID") REFERENCES "Comments"("CommentID")
+);
+
+CREATE TABLE "Hashtag" (
+    "HashtagID" INTEGER,
+    "Content" VARCHAR(50),
+    PRIMARY KEY("HashtagID")
+);
