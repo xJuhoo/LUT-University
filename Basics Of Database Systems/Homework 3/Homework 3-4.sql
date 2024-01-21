edad73940d406b9c012278bbd520b2f351b6800d
Juho Rekonen <128222910+xJuhoo@users.noreply.github.com>
2024-01-21 19:30:53 +0200

    Deletion

--- Basics Of Database Systems/Homework 3/Homework 3-4.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW Comments_of_comments AS
-SELECT 
-    (SELECT Username FROM User WHERE User.UserID = Comments.UserID) AS User,
-    Content AS Comment,
-    FK_CommentID AS "Commented on"
-FROM
-    Comments
-WHERE FK_CommentID IS NOT NULL
-ORDER BY User;
--- Basics Of Database Systems/Homework 3/Homework 3-5.sql
+++
@@ -1,8 +0,0 @@
-CREATE TRIGGER hashtag_not_allowed
-BEFORE INSERT ON Hashtag
-BEGIN 
-    SELECT CASE
-        WHEN NEW.Content LIKE "%mayonnaise%" THEN
-            RAISE (ABORT, "Mayonnaise detected!")
-   END;
-END;
